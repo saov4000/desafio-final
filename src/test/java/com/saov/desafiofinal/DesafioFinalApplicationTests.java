05a1a433d20c3610c806f0e0ab314dcfc5ea9308
saov4000 <saov4000@github.com>
2023-09-17 11:25:43 -0300

    Initial commit

---
+++ src/test/java/com/saov/desafiofinal/DesafioFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saov.desafiofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
