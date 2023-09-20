f94cbc579a57c7865ff203875c43e8d11abaaf66
saov4000 <saov4000@github.com>
2023-09-20 16:25:14 -0300

    fitth commit

--- src/main/java/com/saov/desafiofinal/DesafioFinalApplication.java
+++ src/main/java/com/saov/desafiofinal/DesafioFinalApplication.java
@@ -1,8 +1,12 @@
 package com.saov.desafiofinal;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.annotations.OpenAPI30;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description="Default Server  URL")})
 @SpringBootApplication
 public class DesafioFinalApplication {
 
